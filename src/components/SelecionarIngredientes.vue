0b99ce782d9dcb3ddaf1937427510a76ab38c77b
patricchubaca <patric_job@outlook.com>
2024-05-29 21:46:56 -0300

    Cookin-up :: GitHub Pages

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -2,24 +2,24 @@
 export default {}
 </script>
 <template>
-    <section class="selecionar-ingredientes">
-      <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
-  
-      <p class="paragrafo-lg instrucoes">
-        Selecione abaixo os ingredientes que você quer usar nesta receita:
-      </p>
-  
-      <ul class="categorias">
-  
-      </ul>
-  
-      <p class="paragrafo dica">
-        *Atenção: consideramos que você tem em casa sal, pimenta e água.
-      </p>
-    </section>
-  </template>
-  
-  <style scoped>
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+
+    <p class="paragrafo-lg instrucoes">
+      Selecione abaixo os ingredientes que você quer usar nesta receita:
+    </p>
+
+    <ul class="categorias">
+
+    </ul>
+
+    <p class="paragrafo dica">
+      *Atenção: consideramos que você tem em casa sal, pimenta e água.
+    </p>
+  </section>
+</template>
+
+<style scoped>
 .selecionar-ingredientes {
   display: flex;
   flex-direction: column;
